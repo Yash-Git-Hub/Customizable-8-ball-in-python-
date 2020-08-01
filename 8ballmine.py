e25d9a329ca20572a2cdffa1fe70d0f70db57ac2
Yash-Git-Hub <69065839+Yash-Git-Hub@users.noreply.github.com>
2020-08-01 09:01:47 +0530

    Add files via upload

---
+++ 8ballmine.py
@@ -0,0 +1,24 @@
+def answer(num):
+    if(num==1):
+        return 'its certain'
+    elif(num==2):
+        return 'listen to your heart'
+    elif(num==3):
+        return 'ask again need to think '
+    elif(num==4):
+        return 'no action is the best action '
+    elif(num==5):
+        return 'sleep over it '
+    elif(num==6):
+        return 'no dont are you mad!!!'
+    elif(num==7):
+        return 'Whatever the heart says'
+i=0
+while(i<2):
+    from random import randint
+    print('ready to know your fate ?\n type your question !!:')
+    x=input()
+    r=randint(1,7)
+    print(answer(r))
+    print("\n To ask again press 1 :  \n To quit press two ")
+    i=int(input())
---
+++ Readme.txt
@@ -0,0 +1,3 @@
+1.The prompts can be changed easily. 
+2. Enjoy 
+3.For beginers the code does use  an external function (Def num())
